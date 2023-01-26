284ad8ae3f20dce372f4f2d28bcd3d22e095bd5e
drich629 <123689556+drich629@users.noreply.github.com>
2023-01-26 15:38:53 -0600

    Update streamlit_app.py
    added space between emoji and first word for lines 7 & 8

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boriled Free-Range Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boriled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
